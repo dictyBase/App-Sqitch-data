dbfdab5c9baaf7a5e1c5e8c5dca39e22c17de225
Yogesh Pandit <yogesh.pandit@northwestern.edu>
2013-06-25 10:52:30 -0500

    Using IPC::Cmd to check if required sys commands are available

--- lib/App/Sqitch/DataHandler/sqlite.pm
+++ lib/App/Sqitch/DataHandler/sqlite.pm
@@ -3,19 +3,22 @@ use strict;
 
 package App::Sqitch::DataHandler::sqlite;
 
+use Carp;
+use IPC::Cmd qw[can_run];
 use Mouse;
 
 sub dump {
     my ( $self, $db, $data_dir, $change_id ) = @_;
-    my $cmd = "cp $db $data_dir/$change_id.dump";
-    return $cmd;
+    my $cmd = can_run('cp') or croak 'cp not in PATH';
+    my $full_cmd = "$cmd $db $data_dir/$change_id.dump";
+    return $full_cmd;
 }
 
 sub load {
     my ( $self, $db, $data_dir, $change_id ) = @_;
-    my $cmd = "cp $data_dir/$change_id.dump $db";
-    return $cmd;
-
+    my $cmd = can_run('cp') or croak 'cp not in PATH';
+    my $full_cmd = "cp $data_dir/$change_id.dump $db";
+    return $full_cmd;
 }
 
 1;
